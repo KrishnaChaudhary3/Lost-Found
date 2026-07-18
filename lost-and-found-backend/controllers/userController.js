7559d26718cb6f995c631b195d4341fc97c39d6b
Krishna Kumari <krishna.hs.chaudhary@gmail.com>
2026-07-18 19:34:10 +0530

    Enable user profile retrieval functionality
    Uncommented the getUserProfile function and its related code.

--- lost-and-found-backend/controllers/userController.js
+++ lost-and-found-backend/controllers/userController.js
@@ -1,23 +1,3 @@
-// const Item = require('../models/Item'); // Assuming item schema is here
-
-// // @desc    Get current user's profile and items
-// // @route   GET /api/users/profile
-// // @access  Private
-// exports.getUserProfile = async (req, res) => {
-//   try {
-//     const user = req.user;
-//     const items = await Item.find({ email: user.email }); // Filter by user's email
-
-//     res.json({
-//       email: user.email,
-//       items: items
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// };
-
 const Item = require('../models/Item');
 
 exports.getUserProfile = async (req, res) => {
